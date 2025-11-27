f22d37094dbffe18ee8ccdc6bd303a4b19569f5f
ratulislam46 <arfanratul46@gmail.com>
2025-11-27 15:49:38 +0600

    Added blogs,page not found routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,17 +1,27 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../Root/RootLayout";
 import Home from "../Page/Home/Home";
+import PageNotFount from "../Components/PageNotFount/PageNotFount";
+import Blogs from "../Page/Blogs/Blogs";
 
 
 export const router = createBrowserRouter([
     {
         path: '/',
         Component: RootLayout,
-        children:[
+        children: [
             {
                 index: true,
                 Component: Home
             },
+            {
+                path: '/blogs',
+                Component: Blogs
+            }
         ]
     },
+    {
+        path: '/*',
+        Component: PageNotFount
+    }
 ]);
